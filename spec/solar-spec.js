989b20478f1df471d0e231c2f78353c6c6bb9c36
Catherine Bradley <catherinetybradley@gmail.com>
2018-10-12 17:49:50 -0700

    commit before push - unable to export js and test

--- spec/solar-spec.js
+++ spec/solar-spec.js
@@ -1,9 +1,10 @@
-import { Solar } from './../src/solar.js';
+import { SolarAge } from '../src/solar.js';
 
 describe('daysAlive', function() {
   var today;
   var oneDay;
   var birthday;
+  var ageDays;
   var mercuryDays;
   var venusDays;
   var marsDays;
@@ -13,6 +14,7 @@ describe('daysAlive', function() {
     today = new Date();
     oneDay = 24*60*60*1000; //hours*minutes*seconds*milliseconds - Convert day to milliseconds
     birthday = new Date("10/20/1990");
+    ageDays = ((today.getTime() - birthday.getTime())/(oneDay)).toFixed(1);
   });
 
   beforeEach(function() {
@@ -23,27 +25,30 @@ describe('daysAlive', function() {
   });
 
   it('should return user birthday to as number of days alive', function() {
-    ageDays = ((today.getTime() - birthday.getTime())/(oneDay)).toFixed(1);
     console.log("Age in days:" + ageDays);
+    birthday = new Date("10/20/1990");
+    expect(birthday.getDays()).toEqual(ageDays);
   });
 
   it('should return user age in years to Mercury age in years', function() {
     var ageMercury = (ageDays / mercuryDays).toFixed(1);
-    console.log("Age on Mercury: " + ageMercury);
+    console.log("Age in years on Mercury: " + ageMercury);
   });
 
   it('should return user age in years to Venus age in years', function() {
     var ageVenus = (ageDays / venusDays).toFixed(1);
-    console.log("Age on Venus: " + ageVenus);
+    console.log("Age in years on Venus: " + ageVenus);
   });
 
   it('should return user age in years to Mars age in years', function() {
     var ageMars = (ageDays / marsDays).toFixed(1);
-    console.log("Age on Mars: " + ageMars);
+    console.log("Age in years on Mars: " + ageMars);
   });
 
   it('should return user age in years to Jupiter age in years', function() {
     var ageJupiter = (ageDays / jupiterDays).toFixed(1);
-    console.log("Age on Jupiter: " + ageJupiter);
+    console.log("Age in years on Jupiter: " + ageJupiter);
   });
+
+
 });
--- src/solar.js
+++ src/solar.js
@@ -1,10 +1,40 @@
+export class SolarAge {
+  constructor(birthday) {
+    this.birthday = birthday;
+  }
+
+
+  getDays() {
+  const today = new Date();
+  const oneDay = 24*60*60*1000;
+  let ageDays = ((today.getTime() - this.birthday.getTime())/(oneDay)).toFixed(1);
+  return ageDays;
+  }
+
+
+
+}
+
+
 // export function Solar(birthday) {
 //   this.birthday = birthday;
 // }
 //
-// Solar.prototype.ageCheck = function() {
-//   var today = new Date();
-//   var ageDays = ((today.getTime() - (this.birthday).getTime())/(oneDay)).toFixed(1);
+// const today = new Date();
+// const oneDay = 24*60*60*1000;
 //
-//   return ageDays;
+// const mercuryDays = 88.0;
+// const venusDays = 224.7;
+// const marsDays = 686.7;
+// const jupiterDays = 4331.2;
+//
+// Solar.prototype.getDays = function () {
+// var ageDays = ((today.getTime() - this.birthday.getTime())/(oneDay)).toFixed(1)
+// console.log(ageDays);
+// return ageDays;
 // };
+//
+// //Days in a year for Mercury, Venus, Mars, Jupiter
+// const solarDays = [88.0, 224.7, 686.7, 4331.2]
+// //Average US life expectancy
+// const lifeExp = 79;
